a6d52d1972860a0046db181f34e7c5d8099664a7
Patrick Tappe <pattappe@gmail.com>
2020-08-31 22:12:07 -0400

    1.5

--- courseinfo/src/index.js
+++ courseinfo/src/index.js
@@ -6,8 +6,9 @@ import Total from './Total';
 
 const App = () => {
 
-    const course = 'Half Stack application development'
-    const parts = [
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
       {
         name: 'Fundamentals of React',
         exercises: 10
@@ -21,12 +22,13 @@ const App = () => {
         exercises: 14
       }
     ]
+  }
 
   return (
     <div>
-      <Header course={ course }/>
-      <Content parts = { parts } />
-      <Total parts = { parts } />
+      <Header course={ course.name }/>
+      <Content parts = { course.parts } />
+      <Total parts = { course.parts } />
     </div>
   )
 }
