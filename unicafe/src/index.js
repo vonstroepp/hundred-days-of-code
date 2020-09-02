b9f24c2ee6c511592e389b4d8c3fdd9023590633
Patrick Tappe <pattappe@gmail.com>
2020-09-02 17:46:51 -0400

    1.8

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -3,55 +3,44 @@ import ReactDOM from 'react-dom'
 
 const App = (props) => {
   const [good, setGood] = useState(0);
-  const [bad, setBad] = useState(0);
   const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
   const [all, setAll] = useState(0);
-  const [average, setAverage] = useState(0);
-  const [positive, setPositive] = useState(0);
+
+  const calcAverage = function(a, b, c) {
+    return (a + b + c) / 2 + ' %'
+  }
+  const average = calcAverage(good, neutral, bad);
+
+  const calcPositive = function(all, good){
+    var outOff = all;
+    var value = good;
+    var result = ((value * 100) / outOff) ;
+    if(isNaN(result)) 
+    {
+      return result = 'Not calculated Yet'
+    }
+    return result + ' %';
+  }
   
-  const handleGoodClick = () => {
+  const positive = calcPositive(all, good);
+
+  
+  const handleGoodClick = () => { 
     setGood(good+1);
     setAll(all+1);
-    setAverage(good+0-bad/3);
-    setPositive(()=>{
-      let outOff = all;
-      let value = good;
-      let result = (value * 100) / outOff;
-      return result      
-    });
   }
-  console.log(positive)
 
   const handleBadClick = () => {
     setBad(bad+1);
     setAll(all+1);
-    setAverage(good+0-bad/3)
-    setPositive(()=>{
-      let outOff = good+neutral+bad;
-      let value = good;
-      let result = (value * 100) / outOff;
-      return result
-    });
   }
 
   const handleNeutralClick = () => {
     setNeutral(neutral+1);
     setAll(all+1);
-    setAverage((good+0-bad)/3)
-    setPositive(()=>{
-      let outOff = good+neutral+bad;
-      let value = good;
-      let result = (value * 100) / outOff;
-      return result
-    });
   }
 
-  /*
-    let outOff = good+neutral+bad;
-    let value = good;
-    let result = (value * 100) / outOff;
-  */
-  
   return (
     <div>
       <h2>Give Feedback</h2>
@@ -60,22 +49,33 @@ const App = (props) => {
         <button onClick={ handleNeutralClick }>Neutral</button>
         <button onClick={ handleBadClick }>Bad</button>
       </div> 
-      <h2>Statistics</h2>
-      <div>
-        <div>Good: { good }</div>
-        <div>Neutral: { neutral }</div>
-        <div>Bad: { bad }</div>
-        <div>All: { all }</div>
-        <div>Average: { average }</div>
-        <div>Positive: { positive }</div>
-      </div> 
+      <Statistics good = { good } 
+                  neutral = { neutral }
+                  bad = { bad }
+                  all ={ all }
+                  average = { average }
+                  positive = { positive }
+                  />
     </div>
   )
-
-  
-
-  
 }  
+
+  const Statistics = (props) => {
+    return(
+      <div>
+        <h2>Statistics</h2>
+        <div>
+          <div>Good: { props.good }</div>
+          <div>Neutral: { props.neutral }</div>
+          <div>Bad: { props.bad }</div>
+          <div>All: { props.all }</div>
+          <div>Average: { props.average }</div>
+          <div>Positive: {props.positive }</div>
+        </div> 
+      </div>
+    )
+  }
+
   ReactDOM.render(<App />, 
     document.getElementById('root')
 )
