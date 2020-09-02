fe516ce92346d7e132e4256f8fb48b4456a7df41
Patrick Tappe <pattappe@gmail.com>
2020-09-02 14:37:27 -0400

    1.7

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -1,19 +1,49 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+const App = (props) => {
+
+  const [clicks, setClicks] = useState({
+    good: 0, neutral: 0, bad: 0
+  })
+
+  const handleGoodClick = () => { setClicks ({ ...clicks, good: clicks.good + 1 })}
+  const handleNeutralClick = () => { setClicks ({ ...clicks, neutral: clicks.neutral + 1 })}
+  const handleBadClick = () => { setClicks ({ ...clicks, bad: clicks.bad + 1 })}
+  const allClicks = clicks.good + clicks.bad + clicks.neutral;
+  const averageScore = (a,b,c) => c === 0 ? a : ((a + b - c) / 3);
+  const percentageOfGoodClicks = (a,b) => {
+    let result;
+    if( !a ) { return result = "Not Defined" };
+      let outOff = a+b;
+      let value = a;
+      result = (value * 100) / outOff;
+    return result + ' %'
+  }
+  const percentage = percentageOfGoodClicks(clicks.good, clicks.bad);
+
+  
+  
+  const average = averageScore(clicks.good, 0, clicks.bad);
 
   return (
     <div>
-      code here
+      <button onClick={handleGoodClick}>Good</button>
+      <button onClick={handleNeutralClick}>Neutral</button>
+      <button onClick={handleBadClick}>Bad</button>
+      <div>
+        <h2>Statistics:</h2>
+        <div>Good: { clicks.good }</div>
+        <div>Neutral: { clicks.neutral }</div>
+        <div>Bad:{ clicks.bad }</div>
+        <div>All: { allClicks }</div>
+        <div>Average: { average }</div>
+        <div>Percentage of good: { percentage }</div>
+      </div>
     </div>
   )
-}
-
-ReactDOM.render(<App />, 
-  document.getElementById('root')
+}  
+  ReactDOM.render(<App />, 
+    document.getElementById('root')
 )
+
