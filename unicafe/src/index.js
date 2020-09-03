8e516db3de7d3a7555c9fca1b06c5b7ff020947a
Patrick Tappe <pattappe@gmail.com>
2020-09-02 21:27:32 -0400

    1.13

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -6,12 +6,16 @@ const App = (props) => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
   const [all, setAll] = useState(0);
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState({
+     0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0 
+  })
 
   const calcAverage = function(a, b, c) {
     return (a + b + c) / 2 + ' %'
   }
   const average = calcAverage(good, neutral, bad);
-
+  
   const calcPositive = function(all, good){
     var outOff = all;
     var value = good;
@@ -24,28 +28,50 @@ const App = (props) => {
   }
   
   const positive = calcPositive(all, good);
-
+  
   
   const handleGoodClick = () => { 
     setGood(good+1);
     setAll(all+1);
   }
-
+  
   const handleBadClick = () => {
     setBad(bad+1);
     setAll(all+1);
   }
-
+  
   const handleNeutralClick = () => {
     setNeutral(neutral+1);
     setAll(all+1);
   }
-
+  
+  const handleShowRandomAnecdote = () => {
+    const len = anecdotes.length;
+    let randomNum = Math.floor(Math.random()*len);
+    setSelected(randomNum);
+  }
+  
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ];
+  
+  const handleVote = () => {
+    const newVotes = { ...votes }
+    newVotes[selected] +=1
+    setVotes(newVotes)
+  }
+  
   return (
     <div>
-      <h2>Give Feedback</h2>
-      <div>
-        <Button handleClick = { handleGoodClick } text="Good"/>
+    <Anecdotes numOfVotes = { votes[selected] } handleVote = {handleVote} anecdotes = {anecdotes } selected= { selected } clickHandler = { handleShowRandomAnecdote } text="Show Random Anecdote"/>
+    <h2>Give Feedback</h2>
+    <div>
+    <Button handleClick = { handleGoodClick } text="Good"/>
         <Button handleClick = { handleNeutralClick } text="Neutral"/>
         <Button handleClick = { handleBadClick } text="Bad"/>
       </div> 
@@ -97,6 +123,26 @@ const App = (props) => {
       {text}
     </button>
   )
+
+  const Anecdotes = (props) => {
+    console.log(props);
+    return (
+      <div>
+        <Button handleClick = { props.clickHandler } text = { props.text }/>
+        <Anecdote numOfVotes = {props.numOfVotes} handleVote = {props.handleVote} anecdotes = { props.anecdotes } selected = { props.selected } />
+      </div>
+    )
+  }
+
+  const Anecdote = ({numOfVotes, handleVote, anecdotes, selected}) => (
+    <div>
+      <p>{anecdotes[selected]} <code> has { numOfVotes } votes</code></p>
+      
+      <Button handleClick = {handleVote} text="vote"/>
+
+    </div>
+  )
+  
 
   ReactDOM.render(<App />, 
     document.getElementById('root')
